711e676a9398756b8fea29da521cad9ef1841819
vardhan399 <anurag.vard05@gmail.com>
2024-04-16 12:39:59 +0530

    first commit

---
+++ cl3.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
+public class cl3 {
+    public static void main(String[] args) {
+        
+        // Corrected file path string
+        File f = new File("C:\\Users\\anura\\Desktop\\Angular\\Java3.txt");
+        
+        try {
+            if (f.createNewFile()) {
+                System.out.println("File created");
+            } else {
+                System.out.println("File already exists");
+            }
+        } catch (IOException e) {
+            System.out.println("Exception handled: " + e.getMessage());
+        }
+    }
+}
+
